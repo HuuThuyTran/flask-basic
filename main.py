e1cf8f457d5292548bc8ec657ee6b9d595662089
huuthuy <tranhuuthuy2003@gmail.com>
2026-01-03 16:48:01 +0700

    init: project-flask-baisc

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-a = 10
-print(a)
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'hello flask'
+
+@app.route('/about')
+def about():
+    return 'about page'
+
+@app.route('/contact')
+def contact():
+    return 'contact page'
+
+if __name__ == '__main__':
+    app.run(debug=True)
