2fdf7a489da9d0e05ef7c740440aa7cc5d95e7c6
huuthuy <tranhuuthuy2003@gmail.com>
2026-01-05 21:56:57 +0700

    lesson: template part 1

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-from flask import Flask
+# import các class từ thư viện flask
+from flask import Flask, render_template
 
+# Khởi tạo app Flask từ file hiện tại
 app = Flask(__name__)
 
+# Viết các route cho app
 @app.route('/')
 def home():
-    return 'hello flask'
+    return render_template('home.html')
 
 @app.route('/about')
 def about():
-    return 'about page'
+    return render_template('about.html')
 
 @app.route('/contact')
 def contact():
-    return 'contact page'
+    return render_template('contact.html')
 
+# Chạy ứng dụng Flask
 if __name__ == '__main__':
     app.run(debug=True)
