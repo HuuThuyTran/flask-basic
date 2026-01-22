ddc6779520feb6e277861a92c5561a0183db0e4a
huuthuy <tranhuuthuy2003@gmail.com>
2026-01-23 01:34:52 +0700

    new-knowledge: handles path param & query params

--- salesapp/index.py
+++ salesapp/index.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request
 from salesapp import app
 # from utils import load_categories, load_products # => C1
 import utils # => C2
@@ -11,8 +11,25 @@ def home():
 
 @app.route('/products')
 def product_list():
-    products = utils.load_products()
-    return render_template('products.html', products=products)
+    cate_id = request.args.get('category_id')
+    keyword = request.args.get('keyword')
+    from_price = request.args.get('from_price')
+    to_price = request.args.get('to_price')
+    products = utils.load_products(cate_id, keyword, from_price, to_price)
+
+    category_name = None
+    categories = utils.load_categories()
+    if cate_id:
+        for c in categories:
+            if c['id'] == int(cate_id):
+                category_name = c['name']
+                break
+    return render_template('products.html', products=products, category_name=category_name)
+
+@app.route('/products/<int:product_id>')
+def product_detail(product_id):
+    product = utils.get_product_by_id(product_id)
+    return render_template('product_detail.html', product=product)
 
 # Chạy app
 if __name__ == '__main__':
--- salesapp/utils.py
+++ salesapp/utils.py
@@ -11,5 +11,21 @@ def load_categories():
     return read_json(os.path.join(app.root_path, 'data/categories.json'))
 
 # Hàm load products
-def load_products():
-    return read_json(os.path.join(app.root_path, 'data/products.json'))
+def load_products(cate_id=None, keyword=None, from_price=None, to_price=None):
+    products = read_json(os.path.join(app.root_path, 'data/products.json'))
+    if cate_id:
+        products = [p for p in products if p['category_id'] == int(cate_id)]
+    if keyword:
+        products = [p for p in products if p['name'].lower().find(keyword.lower()) >= 0]
+    if from_price:
+        products = [p for p in products if float(p['price']) >= float(from_price)]
+    if to_price:
+        products = [p for p in products if float(p['price']) <= float(to_price)]
+    return products
+
+# Hàm lấy product theo id
+def get_product_by_id(product_id):
+    products = read_json(os.path.join(app.root_path, 'data/products.json'))
+    for p in products:
+        if p['id'] == product_id:
+            return p
